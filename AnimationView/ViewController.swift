b460d89df402efd6445f6dd48f6d89f60e55bdc0
Hoang Van Luyen <vanhoangluyen@gmail.com>
2017-11-14 13:51:01 +0700

    init

--- AnimationView/ViewController.swift
+++ AnimationView/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var animationView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func animatedBtnDidTouch(_ sender: UIButton) {
+        UIView.animate(withDuration: 1, animations: {
+            //Doi mau background view
+//            self.animationView.backgroundColor = .brown
+            //Tang kich thuoc view
+//            self.animationView.frame.size.height += 10
+//            self.animationView.frame.size.width += 10
+            //Thay doi toa do view
+//            self.animationView.frame.origin.y -= 20
+//            }, completion: nil)
+//-------------------------------------------------------
+            self.animationView.backgroundColor = .brown
+            self.animationView.frame.size.width += 10
+            self.animationView.frame.size.height += 10
+        }) {   _ in UIView.animate(withDuration: 1, delay: 0.25, options: [.autoreverse , .repeat], animations: {
+            //self.animationView.frame.origin.y -= 20
+            self.animationView.frame.size.height -= 10
+            self.animationView.frame.size.width -= 10
+            })
+            
+        }
+    }
+    
 
 }
 
